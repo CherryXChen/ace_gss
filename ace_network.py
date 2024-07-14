4cb88b6db5ca65ecf005ce40ea5b7499f1b8aef1
Tianrui Chen <405371415@qq.com>
2024-07-14 18:00:26 +0800

    Update ace_network.py

--- ace_network.py
+++ ace_network.py
@@ -22,7 +22,12 @@ class Encoder(nn.Module):
         super(Encoder, self).__init__()
 
         self.out_channels = out_channels
-
+        
+        # ctr: change stride
+        # self.conv1 = nn.Conv2d(1, 32, 3, 1, 1)
+        # self.conv2 = nn.Conv2d(32, 64, 3, 1, 1)
+        # self.conv3 = nn.Conv2d(64, 128, 3, 1, 1)
+        # self.conv4 = nn.Conv2d(128, 256, 3, 1, 1)
         self.conv1 = nn.Conv2d(1, 32, 3, 1, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 2, 1)
         self.conv3 = nn.Conv2d(64, 128, 3, 2, 1)
@@ -39,16 +44,16 @@ class Encoder(nn.Module):
         self.res2_skip = nn.Conv2d(256, self.out_channels, 1, 1, 0)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        res = F.relu(self.conv4(x))
+        x1 = F.relu(self.conv1(x))
+        x2 = F.relu(self.conv2(x1))
+        x3 = F.relu(self.conv3(x2))
+        x4 = F.relu(self.conv4(x3))
 
-        x = F.relu(self.res1_conv1(res))
+        x = F.relu(self.res1_conv1(x4))
         x = F.relu(self.res1_conv2(x))
         x = F.relu(self.res1_conv3(x))
 
-        res = res + x
+        res = x4 + x
 
         x = F.relu(self.res2_conv1(res))
         x = F.relu(self.res2_conv2(x))
@@ -56,9 +61,338 @@ class Encoder(nn.Module):
 
         x = self.res2_skip(res) + x
 
-        return x
+        return x, (x1, x2, x3, x4)
+
+# Output feature and point cloud.
+class Head(nn.Module):
+    """
+    MLP network predicting per-pixel scene coordinates given a feature vector. All layers are 1x1 convolutions.
+    """
+
+    def __init__(self,
+                 mean,
+                 num_head_blocks,
+                 use_homogeneous,
+                 homogeneous_min_scale=0.01,
+                 homogeneous_max_scale=4.0,
+                 in_channels=512):
+        super(Head, self).__init__()
+
+        self.use_homogeneous = use_homogeneous
+        self.in_channels = in_channels  # Number of encoder features.
+        self.head_channels = 512  # Hardcoded.
+
+        # We may need a skip layer if the number of features output by the encoder is different.
+        self.head_skip = nn.Identity() if self.in_channels == self.head_channels else nn.Conv2d(self.in_channels,
+                                                                                                self.head_channels, 1,
+                                                                                                1, 0)
+
+        self.res3_conv1 = nn.Conv2d(self.in_channels, self.head_channels, 1, 1, 0)
+        self.res3_conv2 = nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0)
+        self.res3_conv3 = nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0)
+
+        self.res_blocks = []
+
+        for block in range(num_head_blocks):
+            self.res_blocks.append((
+                nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0),
+                nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0),
+                nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0),
+            ))
+
+            super(Head, self).add_module(str(block) + 'c0', self.res_blocks[block][0])
+            super(Head, self).add_module(str(block) + 'c1', self.res_blocks[block][1])
+            super(Head, self).add_module(str(block) + 'c2', self.res_blocks[block][2])
+
+        self.fc1 = nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0)
+        self.fc2 = nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0)
+
+        if self.use_homogeneous:
+            self.fc3 = nn.Conv2d(self.head_channels, 4, 1, 1, 0)
+
+            # Use buffers because they need to be saved in the state dict.
+            self.register_buffer("max_scale", torch.tensor([homogeneous_max_scale]))
+            self.register_buffer("min_scale", torch.tensor([homogeneous_min_scale]))
+            self.register_buffer("max_inv_scale", 1. / self.max_scale)
+            self.register_buffer("h_beta", math.log(2) / (1. - self.max_inv_scale))
+            self.register_buffer("min_inv_scale", 1. / self.min_scale)
+        else:
+            self.fc3 = nn.Conv2d(self.head_channels, 3, 1, 1, 0)
+
+        # Learn scene coordinates relative to a mean coordinate (e.g. center of the scene).
+        self.register_buffer("mean", mean.clone().detach().view(1, 3, 1, 1))
+
+    def forward(self, res):
+
+        x = F.relu(self.res3_conv1(res))
+        x = F.relu(self.res3_conv2(x))
+        x = F.relu(self.res3_conv3(x))
+
+        res = self.head_skip(res) + x
+
+        for res_block in self.res_blocks:
+            x = F.relu(res_block[0](res))
+            x = F.relu(res_block[1](x))
+            x = F.relu(res_block[2](x))
+
+            res = res + x
+
+        sc = F.relu(self.fc1(res))
+        sc_out = F.relu(self.fc2(sc))
+        sc = self.fc3(sc_out)
+
+        if self.use_homogeneous:
+            # Dehomogenize coords:
+            # Softplus ensures we have a smooth homogeneous parameter with a minimum value = self.max_inv_scale.
+            h_slice = F.softplus(sc[:, 3, :, :].unsqueeze(1), beta=self.h_beta.item()) + self.max_inv_scale
+            h_slice.clamp_(max=self.min_inv_scale)
+            sc = sc[:, :3] / h_slice
+
+        # Add the mean to the predicted coordinates.
+        sc += self.mean
+
+        return sc, sc_out
 
 
+# raft upsample_flow
+class Upsample(nn.Module):
+    """
+    Upsampling point cloud.
+    """
+
+    def __init__(self,
+                 in_channels=512):
+        super(Upsample, self).__init__()
+
+        self.mask = nn.Sequential(
+            nn.Conv2d(in_channels, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, (8**2)*9, 1, padding=0))
+
+    def upsample_flow(self, flow, mask):
+        """ Upsample flow field [H/8, W/8, 2] -> [H, W, 2] using convex combination """
+        N, D, H, W = flow.shape
+        factor = 8
+        mask = mask.view(N, 1, 9, factor, factor, H, W)
+        mask = torch.softmax(mask, dim=2)
+
+        up_flow = F.unfold(flow, [3,3], padding=1)
+        up_flow = up_flow.view(N, D, 9, 1, 1, H, W)
+
+        up_flow = torch.sum(mask * up_flow, dim=2)
+        up_flow = up_flow.permute(0, 1, 4, 2, 5, 3)
+        return up_flow.reshape(N, D, factor*H, factor*W)
+    
+    def forward(self, x, feature):
+
+        up_mask = .25 * self.mask(feature)
+        x_up = self.upsample_flow(x, up_mask)
+
+        return x_up
+
+class GSRegressor(nn.Module):
+    def __init__(self):
+        super(GSRegressor, self).__init__()
+
+        self.head_dim = 32
+
+        encoder_dims = [32, 64, 128, 256]
+        decoder_dims = [256, 128, 64, 32]
+
+        self.gs_encoder1 = nn.Sequential(
+            nn.Conv2d(3, encoder_dims[0], 3, 1, 1),
+            nn.ReLU(inplace=True)
+        )
+        self.gs_encoder2 = nn.Sequential(
+            nn.Conv2d(encoder_dims[0], encoder_dims[1], 3, 2, 1),
+            nn.ReLU(inplace=True)
+        )
+        self.gs_encoder3 = nn.Sequential(
+            nn.Conv2d(encoder_dims[1], encoder_dims[2], 3, 2, 1),
+            nn.ReLU(inplace=True)
+        )
+        self.gs_encoder4 = nn.Sequential(
+            nn.Conv2d(encoder_dims[2], encoder_dims[3], 3, 2, 1),
+            nn.ReLU(inplace=True)
+        )
+
+        self.gs_decoder4 = nn.Sequential(
+            nn.Conv2d(encoder_dims[3] * 2, decoder_dims[0], 3, 1, 1),
+            nn.ReLU(inplace=True)
+        )
+        self.gs_decoder3 = nn.Sequential(
+            nn.Conv2d(encoder_dims[2] * 2 + decoder_dims[0], decoder_dims[1], 3, 1, 1),
+            nn.ReLU(inplace=True)
+        )
+        self.gs_decoder2 = nn.Sequential(
+            nn.Conv2d(encoder_dims[1] * 2 + decoder_dims[1], decoder_dims[2], 3, 1, 1),
+            nn.ReLU(inplace=True)
+        )
+        self.gs_decoder1 = nn.Sequential(
+            nn.Conv2d(encoder_dims[0] * 2 + decoder_dims[2], decoder_dims[3], 3, 1, 1),
+            nn.ReLU(inplace=True)
+        )
+
+        self.gs_up = nn.Upsample(scale_factor=2, mode="bilinear")
+
+        self.gs_out_conv = nn.Sequential(
+            nn.Conv2d(decoder_dims[3]+1+3, self.head_dim, kernel_size=3, padding=1),  # 1:gray image, 3: point cloud
+            nn.ReLU(inplace=True)
+        )
+
+        self.gs_rot_head = nn.Sequential(
+            nn.Conv2d(self.head_dim, self.head_dim, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.head_dim, 4, kernel_size=1),
+        )
+        self.gs_scale_head = nn.Sequential(
+            nn.Conv2d(self.head_dim, self.head_dim, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.head_dim, 3, kernel_size=1),
+            nn.Softplus(beta=100)
+        )
+        self.gs_opacity_head = nn.Sequential(
+            nn.Conv2d(self.head_dim, self.head_dim, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(self.head_dim, 1, kernel_size=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, img, point_cloud, img_feat):
+        img_feat1, img_feat2, img_feat3, img_feat4 = img_feat
+        pc_feat1 = self.gs_encoder1(point_cloud)
+        pc_feat2 = self.gs_encoder2(pc_feat1)
+        pc_feat3 = self.gs_encoder3(pc_feat2)
+        pc_feat4 = self.gs_encoder4(pc_feat3)
+
+        feat4 = torch.concat([img_feat4, pc_feat4], dim=1)
+        feat3 = torch.concat([img_feat3, pc_feat3], dim=1)
+        feat2 = torch.concat([img_feat2, pc_feat2], dim=1)
+        feat1 = torch.concat([img_feat1, pc_feat1], dim=1)
+
+        up4 = self.gs_decoder4(feat4)
+        up4 = self.gs_up(up4)
+        up3 = self.gs_decoder3(torch.cat([up4, feat3], dim=1))
+        up3 = self.gs_up(up3)
+        up2 = self.gs_decoder2(torch.cat([up3, feat2], dim=1))
+        up2 = self.gs_up(up2)
+        up1 = self.gs_decoder1(torch.cat([up2, feat1], dim=1))
+
+        out = torch.cat([up1, img, point_cloud], dim=1)
+        
+        out = self.gs_out_conv(out)
+
+        # rot head
+        rot_out = self.gs_rot_head(out)
+        # if torch.isnan(rot_out).any():
+        #     _logger.info(f'=============3')
+        rot_out = torch.nn.functional.normalize(rot_out, dim=1)
+
+        # scale head
+        scale_out = torch.clamp_max(self.gs_scale_head(out), 0.01)
+
+        # opacity head
+        opacity_out = self.gs_opacity_head(out)
+        # opacity_out = torch.ones_like(scale_out[:, 0, None, :, :])
+        # _logger.info(f"------------------------------------------------")
+        # _logger.info(f"{torch.max(opacity_out)}, {torch.min(opacity_out)}, {torch.mean(opacity_out)}, {torch.median(opacity_out)}")
+
+        return rot_out, scale_out, opacity_out
+
+'''
+# Head + 8x upsampling
+class Head(nn.Module):
+    """
+    MLP network predicting per-pixel scene coordinates given a feature vector. All layers are 1x1 convolutions.
+    """
+
+    def __init__(self,
+                 mean,
+                 num_head_blocks,
+                 use_homogeneous,
+                 homogeneous_min_scale=0.01,
+                 homogeneous_max_scale=4.0,
+                 in_channels=512):
+        super(Head, self).__init__()
+
+        self.use_homogeneous = use_homogeneous
+        self.in_channels = in_channels  # Number of encoder features.
+        self.head_channels = 512  # Hardcoded.
+
+        # We may need a skip layer if the number of features output by the encoder is different.
+        self.head_skip = nn.Identity() if self.in_channels == self.head_channels else nn.Conv2d(self.in_channels,
+                                                                                                self.head_channels, 1,
+                                                                                                1, 0)
+
+        self.res3_conv1 = nn.Conv2d(self.in_channels, self.head_channels, 1, 1, 0)
+        self.res3_conv2 = nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0)
+        self.res3_conv3 = nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0)
+
+        self.res_blocks = []
+
+        for block in range(3):  # num_head_blocks
+            self.res_blocks.append((
+                nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0),
+                nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0),
+                nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0),
+            ))
+
+            super(Head, self).add_module(str(block) + 'c0', self.res_blocks[block][0])
+            super(Head, self).add_module(str(block) + 'c1', self.res_blocks[block][1])
+            super(Head, self).add_module(str(block) + 'c2', self.res_blocks[block][2])
+
+        self.up = nn.Upsample(scale_factor=2, mode="bilinear")
+        self.fc1 = nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0)
+        self.fc2 = nn.Conv2d(self.head_channels, self.head_channels, 1, 1, 0)
+
+        if self.use_homogeneous:
+            self.fc3 = nn.Conv2d(self.head_channels, 4, 1, 1, 0)
+
+            # Use buffers because they need to be saved in the state dict.
+            self.register_buffer("max_scale", torch.tensor([homogeneous_max_scale]))
+            self.register_buffer("min_scale", torch.tensor([homogeneous_min_scale]))
+            self.register_buffer("max_inv_scale", 1. / self.max_scale)
+            self.register_buffer("h_beta", math.log(2) / (1. - self.max_inv_scale))
+            self.register_buffer("min_inv_scale", 1. / self.min_scale)
+        else:
+            self.fc3 = nn.Conv2d(self.head_channels, 3, 1, 1, 0)
+
+        # Learn scene coordinates relative to a mean coordinate (e.g. center of the scene).
+        self.register_buffer("mean", mean.clone().detach().view(1, 3, 1, 1))
+
+    def forward(self, res):
+
+        x = F.relu(self.res3_conv1(res))
+        x = F.relu(self.res3_conv2(x))
+        x = F.relu(self.res3_conv3(x))
+
+        res = self.head_skip(res) + x
+
+        for res_block in self.res_blocks:
+            x = F.relu(res_block[0](res))
+            x = F.relu(res_block[1](x))
+            x = F.relu(res_block[2](x))
+            res = res + x
+            res = self.up(res)
+
+        sc = F.relu(self.fc1(res))
+        sc = F.relu(self.fc2(sc))
+        sc = self.fc3(sc)
+
+        if self.use_homogeneous:
+            # Dehomogenize coords:
+            # Softplus ensures we have a smooth homogeneous parameter with a minimum value = self.max_inv_scale.
+            h_slice = F.softplus(sc[:, 3, :, :].unsqueeze(1), beta=self.h_beta.item()) + self.max_inv_scale
+            h_slice.clamp_(max=self.min_inv_scale)
+            sc = sc[:, :3] / h_slice
+
+        # Add the mean to the predicted coordinates.
+        sc += self.mean
+
+        return sc
+'''    
+
+'''
 class Head(nn.Module):
     """
     MLP network predicting per-pixel scene coordinates given a feature vector. All layers are 1x1 convolutions.
@@ -147,7 +481,7 @@ class Head(nn.Module):
         sc += self.mean
 
         return sc
-
+'''
 
 class Regressor(nn.Module):
     """
@@ -155,8 +489,8 @@ class Regressor(nn.Module):
 
     The network predicts a 3d scene coordinates, the output is subsampled by a factor of 8 compared to the input.
     """
-
-    OUTPUT_SUBSAMPLE = 8
+    # ctr: change from 8 to 1
+    OUTPUT_SUBSAMPLE = 1
 
     def __init__(self, mean, num_head_blocks, use_homogeneous, num_encoder_features=512):
         """
@@ -173,6 +507,8 @@ class Regressor(nn.Module):
 
         self.encoder = Encoder(out_channels=self.feature_dim)
         self.heads = Head(mean, num_head_blocks, use_homogeneous, in_channels=self.feature_dim)
+        self.upsample = Upsample()
+        self.gs_regressor = GSRegressor()
 
     @classmethod
     def create_from_encoder(cls, encoder_state_dict, mean, num_head_blocks, use_homogeneous):
@@ -199,7 +535,7 @@ class Regressor(nn.Module):
         return regressor
 
     @classmethod
-    def create_from_state_dict(cls, state_dict):
+    def create_from_state_dict(cls, encoder_state_dict, heads_dict, upsample_dict, gs_dict):
         """
         Instantiate a regressor from a pretrained state dictionary.
 
@@ -209,14 +545,15 @@ class Regressor(nn.Module):
         mean = torch.zeros((3,))
 
         # Count how many head blocks are in the dictionary.
-        pattern = re.compile(r"^heads\.\d+c0\.weight$")
-        num_head_blocks = sum(1 for k in state_dict.keys() if pattern.match(k))
+        # pattern = re.compile(r"^heads\.\d+c0\.weight$")
+        # num_head_blocks = sum(1 for k in state_dict.keys() if pattern.match(k))
+        num_head_blocks = 1
 
         # Whether the network uses homogeneous coordinates.
-        use_homogeneous = state_dict["heads.fc3.weight"].shape[0] == 4
+        use_homogeneous = heads_dict["fc3.weight"].shape[0] == 4
 
         # Number of output channels of the last encoder layer.
-        num_encoder_features = state_dict['encoder.res2_conv3.weight'].shape[0]
+        num_encoder_features = encoder_state_dict['res2_conv3.weight'].shape[0]
 
         # Create a regressor.
         _logger.info(f"Creating regressor from pretrained state_dict:"
@@ -225,8 +562,13 @@ class Regressor(nn.Module):
                      f"\n\tEncoder feature size: {num_encoder_features}")
         regressor = cls(mean, num_head_blocks, use_homogeneous, num_encoder_features)
 
-        # Load all weights.
-        regressor.load_state_dict(state_dict)
+        # Load weights.
+        regressor.encoder.load_state_dict(encoder_state_dict)
+        regressor.heads.load_state_dict(heads_dict)
+        if len(upsample_dict):
+            regressor.upsample.load_state_dict(upsample_dict)
+        if len(gs_dict):
+            regressor.gs_regressor.load_state_dict(gs_dict)
 
         # Done.
         return regressor
@@ -239,17 +581,22 @@ class Regressor(nn.Module):
         encoder_state_dict: encoder state dictionary
         head_state_dict: scene-specific head state dictionary
         """
-        # We simply merge the dictionaries and call the other constructor.
-        merged_state_dict = {}
 
-        for k, v in encoder_state_dict.items():
-            merged_state_dict[f"encoder.{k}"] = v
+        heads_dict = {}
+        upsample_dict = {}
+        gs_dict = {}
 
         for k, v in head_state_dict.items():
-            merged_state_dict[f"heads.{k}"] = v
+            if 'mask' in k:
+                upsample_dict[k] = v
+            elif 'gs' in k:
+                gs_dict[k] = v 
+            else:
+                heads_dict[k] = v
 
-        return cls.create_from_state_dict(merged_state_dict)
+        return cls.create_from_state_dict(encoder_state_dict, heads_dict, upsample_dict, gs_dict)
 
+    
     def load_encoder(self, encoder_dict_file):
         """
         Load weights into the encoder network.
@@ -261,10 +608,19 @@ class Regressor(nn.Module):
 
     def get_scene_coordinates(self, features):
         return self.heads(features)
+    
+    def upsample_scene_coordinates(self, x, feature):
+        return self.upsample(x, feature)
+    
+    def get_gs_params(self, img, pc, img_feat):
+        return self.gs_regressor(img, pc, img_feat)
 
     def forward(self, inputs):
         """
         Forward pass.
         """
-        features = self.get_features(inputs)
-        return self.get_scene_coordinates(features)
+        features, img_feat = self.get_features(inputs)
+        sparse_pc, feature_pc = self.get_scene_coordinates(features)
+        pc = self.upsample_scene_coordinates(sparse_pc, feature_pc)
+        rot_maps, scale_maps, opacity_maps = self.get_gs_params(inputs, pc, img_feat)
+        return sparse_pc, pc, rot_maps, scale_maps, opacity_maps
